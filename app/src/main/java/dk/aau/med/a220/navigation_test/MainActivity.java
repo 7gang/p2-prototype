1242cf0cb3cb7d98d926bb9053e3c85ff15699fa
kuff <petergl@protonmail.com>
2019-05-06 22:55:49 +0200

    Initial commit

---
+++ app/src/main/java/dk/aau/med/a220/navigation_test/MainActivity.java
@@ -0,0 +1,54 @@
+package dk.aau.med.a220.navigation_test;
+
+import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.NonNull;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        navView.setOnNavigationItemSelectedListener(this);
+
+        loadFragment(new TowerFragment());
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Fragment fragment = null;
+        switch(menuItem.getItemId()) {
+
+            case R.id.navigation_home:
+                fragment = new TowerFragment();
+                break;
+
+            case R.id.navigation_dashboard:
+                fragment = new CityFragment();
+                break;
+
+            case R.id.navigation_notifications:
+                fragment = new StatsFragment();
+                break;
+
+        }
+        return loadFragment(fragment);
+    }
+}
