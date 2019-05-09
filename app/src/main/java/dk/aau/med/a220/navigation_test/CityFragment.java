45806c5d6f180492e0a581ea02c3afd22d466a48
steffan christensen <stefc5949@gmail.com>
2019-05-09 23:01:20 +0200

    Implement method updateBuilding into CityFragment, Update TitleActivity layout

--- app/src/main/java/dk/aau/med/a220/navigation_test/CityFragment.java
+++ app/src/main/java/dk/aau/med/a220/navigation_test/CityFragment.java
@@ -1,5 +1,8 @@
 package dk.aau.med.a220.navigation_test;
 
+import android.databinding.DataBindingUtil;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,12 +10,51 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+
+import java.util.ArrayList;
+
+import dk.aau.med.a220.navigation_test.databinding.FragmentCityBinding;
 
 public class CityFragment extends Fragment {
-
+    private FragmentCityBinding binding;
+    private Bitmap lv1;
+    private Bitmap lv2;
+    private Bitmap lv3;
+    private ImageView[] imageViewsA = new ImageView[3];
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_city, null);
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_city,container,false);
+        View view = binding.getRoot();
+        lv1 = Bitmap.createBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.buildinglv1));
+        lv2 = Bitmap.createBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.buildinglv2));
+        lv3 = Bitmap.createBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.buildinglv3));
+        imageViewsA[0] = binding.imageView2;
+        imageViewsA[1] = binding.imageView;
+        imageViewsA[2] = binding.imageView3;
+        return view;
+    }
+
+    public void onStart() {
+        // update the user's tower level every time the user navigates to this screen
+        updateBuilding();
+        super.onStart();
+    }
+
+    public void updateBuilding(){
+        for(int i =0 ;i<=2;i++){
+            switch (Game.getLevels()[i]){
+                case 1:
+                    imageViewsA[i].setImageBitmap(lv1);
+                    break;
+                case 2:
+                    imageViewsA[i].setImageBitmap(lv2);
+                    break;
+                case 3:
+                    imageViewsA[i].setImageBitmap(lv3);
+                    break;
+            }}
     }
 }
--- app/src/main/java/dk/aau/med/a220/navigation_test/TowerFragment.java
+++ app/src/main/java/dk/aau/med/a220/navigation_test/TowerFragment.java
@@ -1,5 +1,6 @@
 package dk.aau.med.a220.navigation_test;
 
+import android.app.Activity;
 import android.databinding.DataBindingUtil;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -14,12 +15,19 @@ import android.view.ViewGroup;
 import dk.aau.med.a220.navigation_test.databinding.FragmentTowerBinding;
 
 public class TowerFragment extends Fragment {
-    private FragmentTowerBinding binding;
+   private FragmentTowerBinding binding;
+   private Bitmap lv1;
+   private Bitmap lv2;
+   private Bitmap lv3;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         binding = DataBindingUtil.inflate(inflater,R.layout.fragment_tower,container,false);
         View view = binding.getRoot();
+
+        lv1 = Bitmap.createBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.buildinglv1));
+        lv2 = Bitmap.createBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.buildinglv2));
+        lv3 = Bitmap.createBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.buildinglv3));
         //binding.imageView2.setImageBitmap(Bitmap.createBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.buildinglv2)));
         //updateBuilding(3);
         return view;
@@ -43,13 +51,13 @@ public class TowerFragment extends Fragment {
     public void updateBuilding(int lv){
         switch (lv){
             case 1:
-                binding.imageView2.setImageBitmap(Bitmap.createBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.buildinglv1)));
+                binding.imageView2.setImageBitmap(lv1);
                 break;
             case 2:
-                binding.imageView2.setImageBitmap(Bitmap.createBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.buildinglv2)));
+                binding.imageView2.setImageBitmap(lv2);
                 break;
             case 3:
-                binding.imageView2.setImageBitmap(Bitmap.createBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.buildinglv3)));
+                binding.imageView2.setImageBitmap(lv3);
                 break;
         }
     }
